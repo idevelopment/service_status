54c56ebc3e2e1c81bf4655d45486afb4803b9aea
Tjoosten <Tjoosten3@gmail.com>
2016-04-26 16:20:17 +0200

    Update #27 -> add empty functions

--- app/Http/Controllers/IncidentsController.php
+++ app/Http/Controllers/IncidentsController.php
@@ -8,5 +8,28 @@ use App\Http\Requests;
 
 class IncidentsController extends Controller
 {
-    //
+    public function __constrcut()
+    {
+
+    }
+
+    public function index()
+    {
+
+    }
+
+    public function searchIncidents()
+    {
+
+    }
+
+    public function reportIncident()
+    {
+
+    }
+
+    public function storeIncident()
+    {
+
+    }
 }
