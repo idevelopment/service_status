717929519e48b682c0f11a271d7ef468b184a138
Tjoosten <Tjoosten3@gmail.com>
2016-07-25 23:26:52 +0200

    Fix #51 Add backup system.

--- config/app.php
+++ config/app.php
@@ -156,6 +156,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Spatie\Backup\BackupServiceProvider::class,
 
     ],
 
