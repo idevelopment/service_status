d14c7c21efaf689c041a461f5846223a4442366a
Tjoosten <Tjoosten3@gmail.com>
2016-07-26 04:29:50 +0200

    Fix #63 Build up the label validator.

--- app/Http/Requests/LabelValidator.php
+++ app/Http/Requests/LabelValidator.php
@@ -13,7 +13,7 @@ class LabelValidator extends Request
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,9 @@ class LabelValidator extends Request
     public function rules()
     {
         return [
-            //
+            'color'       => 'required',
+            'name'        => 'required',
+            'description' => 'required',
         ];
     }
 }
