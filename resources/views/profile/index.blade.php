080767f4757cfccbe63851b2437244529ae1249c
Tjoosten <Tjoosten3@gmail.com>
2016-07-27 20:43:17 +0200

    Fix #55 Enable validation errors to the account update views.

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -38,42 +38,66 @@
                                     {{ csrf_field() }}
 
                                     {{-- Name form-group --}}
-                                    <div class="form-group">
+                                    <div class="form-group {{ $errors->has('name') ? ' has-error' : '' }}">
                                         <label for="name" class="col-md-3 control-label">
                                             Name <strong class="text-danger">*</strong>
                                         </label>
                                         <div class="col-md-6">
                                             <input type="text" id="name" name="name" value="{{ $query->name }}" class="form-control">
+
+                                            @if ($errors->has('name'))
+                                                <span class="help-block">
+                                                    <strong>{{ $errors->first('name') }}</strong>
+                                                </span>
+                                            @endif
                                         </div>
                                     </div>
 
                                     {{-- Email form-group --}}
-                                    <div class="form-group">
+                                    <div class="form-group {{ $errors->has('email') ? 'has-error' : '' }}">
                                         <label for="email" class="col-md-3 control-label">
                                             Email: <strong class="text-danger">*</strong>
                                         </label>
                                         <div class="col-md-6">
                                             <input type="text" id="email" name="email" value="{{ $query->email }}" class="form-control">
+
+                                            @if ($errors->has('email'))
+                                                <span class="help-block">
+                                                    <strong>{{ $errors->first('email') }}</strong>
+                                                </span>
+                                            @endif
                                         </div>
                                     </div>
 
                                     {{-- Mobile phone number form-group --}}
-                                    <div class="form-group">
+                                    <div class="form-group {{ $errors->has('mobile_number') ? 'has-error' : '' }}">
                                         <label for="mobilePhone" class="col-md-3 control-label">
                                             Mobile phone <span class="text-danger">*</span>
                                         </label>
                                         <div class="col-md-6">
                                             <input type="text" id="mobilePhone" name="mobile_number" value="{{ $query->mobile_number }}" class="form-control">
+
+                                            @if ($errors->has('mobile_phone'))
+                                                <span class="help-block">
+                                                    <strong>{{ $errors->first('mobile_number') }}</strong>
+                                                </span>
+                                            @endif
                                         </div>
                                     </div>
 
                                     {{-- Office phone number form-group --}}
-                                    <div class="form-group">
+                                    <div class="form-group {{ $errors->has('office_number') ? 'has-error' : '' }}">
                                         <label for="officePhone" class="col-md-3 control-label">
                                             Office phone <span class="text-danger">*</span>
                                         </label>
                                         <div class="col-md-6">
                                             <input type="text" id="mobilePhone" name="office_phone" value="{{ $query->office_number }}" class="form-control">
+
+                                            @if ($errors->has('office_phone'))
+                                                <span class="help-block">
+                                                    <strong>{{ $errors->first('office_number') }}</strong>
+                                                </span>
+                                            @endif
                                         </div>
                                     </div>
 
@@ -122,10 +146,16 @@
                                     {{ csrf_field() }}
 
                                     {{-- Password form-group --}}
-                                    <div class="form-group">
+                                    <div class="form-group {{ $errors->has('password') ? 'has-error' : '' }}">
                                         <label for="password" class="col-md-3 control-label"> Password: </label>
                                         <div class="col-md-6">
                                             <input type="text" id="password" placeholder="Password" name="password" class="form-control">
+
+                                            @if ($errors->has('password'))
+                                                <span class="help-block">
+                                                    <strong>{{ $errors->first('password') }}</strong>
+                                                </span>
+                                            @endif
                                         </div>
                                     </div>
 
